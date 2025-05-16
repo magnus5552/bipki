80ae3d4e505bf3ecb05ba5a26220300d81d7943f
gli <gli@skbkontur.ru>
2025-05-16 22:30:46 +0500

    Add db context and models for auth

---
+++ Bipki.Database.Models/UserModels/Role.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Bipki.Database.Models.UserModels;
+
+public class Role: IdentityRole<Guid>
+{
+    
+}
---
+++ Bipki.Database/BipkiContext.cs
@@ -0,0 +1,9 @@
+﻿using Bipki.Database.Models.UserModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Bipki.Database;
+
+public class BipkiContext : IdentityDbContext<User, Role, Guid>
+{
+    
+}
