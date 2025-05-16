3b286b770017dc8f41d89225a58982f541c2e6c5
gli <gli@skbkontur.ru>
2025-05-16 23:33:26 +0500

    Configure

--- Bipki.App/Program.cs
+++ Bipki.App/Program.cs
@@ -1,22 +1,40 @@
+using Bipki.App.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<ApplicationOptions>(AppOptionsProvider.GetConfiguration());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
+app.UseCors(); 
+app.UsePathBase("/api");
+app.UseRouting();
 
 app.MapControllerRoute(
     name: "default",
