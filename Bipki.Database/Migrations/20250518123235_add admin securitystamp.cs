478c9aa528a8fec5e9650c66596d35cd04e6d76a
gli <gli@skbkontur.ru>
2025-05-18 17:42:51 +0500

    add migration

---
+++ Bipki.Database/Migrations/20250518123235_add admin securitystamp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bipki.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class addadminsecuritystamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("7e0ca8d7-841b-4f0d-92e8-64ed6dd9805a"),
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "ec888857-3fbc-4939-bab2-c127a2bc59a2", "oikamsda" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("7e0ca8d7-841b-4f0d-92e8-64ed6dd9805a"),
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "d5f156dc-4085-42d9-8d7c-6bcad864c174", null });
+        }
+    }
+}
