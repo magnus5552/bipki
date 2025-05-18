0e55c9a7f4f9b2894fb5e7f273d02ce3fccc2a3b
gli <gli@skbkontur.ru>
2025-05-18 15:50:44 +0500

    Refactor admin controller

--- Bipki.App/Features/Admin/AdminController.cs
+++ Bipki.App/Features/Admin/AdminController.cs
@@ -9,29 +9,12 @@ namespace Bipki.App.Features.Admin;
 [Route("admin")]
 public class AdminController : ControllerBase
 {
-    private readonly IConferenceRepository conferenceRepository;
     private readonly IUserRepository userRepository;
 
-    public AdminController(IConferenceRepository conferenceRepository, IUserRepository userRepository)
+    public AdminController(IUserRepository userRepository)
     {
-        this.conferenceRepository = conferenceRepository;
         this.userRepository = userRepository;
     }
-
-    [Authorize(Roles = Roles.User)]
-    [HttpGet("huy")]
-    public async Task<IActionResult> GetHuy()
-    {
-        return Ok("huy");
-    }
-
-    [Authorize(Roles = Roles.Admin)]
-    [HttpGet("huy2")]
-    public async Task<IActionResult> GetHuy2()
-    {
-        return Ok("huy2");
-    }
-    
 
     [Authorize(Roles = Roles.Admin)]
     [HttpPatch("checkInGuest/{userId:guid}")]
